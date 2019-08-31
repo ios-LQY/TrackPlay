911628a8658225483eaa4baf27b657c2dbc008c1
ios-LQY <771642809@qq.com>
2019-08-31 09:10:50 +0800

    Initial Commit

---
+++ LQYTrackPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LQYTrackPlay
+//
+//  Created by 李青洋 on 2019/8/31.
+//  Copyright © 2019 LQY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LQYTrackPlay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LQYTrackPlay
+//
+//  Created by 李青洋 on 2019/8/31.
+//  Copyright © 2019 LQY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
